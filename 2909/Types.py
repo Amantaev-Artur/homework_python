00904876f0bed8ca6394e719d401f4b39220995d
Artur <amantaev_artur@mail.ru>
2021-10-01 13:12:42 +0300

    homework_python

---
+++ 2909/Types.py
@@ -0,0 +1,13 @@
+list = [1,2,3]
+korteg = (1,2,3)
+d = {'name1':'Artur', 'name2':'BOb', 'name3':'Ben'}
+m = {1,2,3,4,5}
+
+print(type(list))
+print(list)
+print(type(korteg))
+print(korteg)
+print(type(d))
+print(d)
+print(type(m))
+print(m)
